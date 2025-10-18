f42b8c67a1f677480de039a22ca4375abbd0ae34
NicolasSossou <sossounicolas846@gmail.com>
2025-10-18 19:38:45 +0200

    Added all of the above but not finished them

--- Infrastrucutre/Models/ProductCreateRequest.cs
+++ Infrastrucutre/Models/ProductCreateRequest.cs
@@ -4,4 +4,6 @@ public class ProductCreateRequest
 {
     public string ProductTitle { get; set; } = string.Empty;
     public decimal ProductPrice { get; set; }
+    public string ProductCategory { get; set; } = null!;
+    public string ProductManufacturer { get; set; } = null!;
 }
