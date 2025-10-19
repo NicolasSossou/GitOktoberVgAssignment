c22710ce6e692a7c45dc2265370667f01062704a
NicolasSossou <sossounicolas846@gmail.com>
2025-10-19 19:47:40 +0200

    Otroligt

---
+++ WpfApp1/Models/ViewModel.cs
@@ -0,0 +1,10 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MainApp.ViewModels;
+
+public partial class ProductViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string _title = "Products";
+}
